4a8c010f037549be6a09bf090fac725921801253
Martin Hendrawan <martin@domain.com>
2022-09-28 10:21:50 +0700

    added feature one

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and Add Feature 001
         </a>
       </header>
     </div>
